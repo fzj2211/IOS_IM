5eb658d99ef998c70e25e916f365c5247aca14bd
炎檬 <fengzhijie@fengzhijiedeMacBook-Pro.local>
2016-08-22 09:29:20 +0800

    增加聊天室

---
+++ YMChat/YMChat/ChatRoomViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatRoomViewController.h
+//  YMChat
+//
+//  Created by 炎檬 on 16/8/22.
+//  Copyright © 2016年 juangua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatRoomViewController : UIViewController
+
+@end
